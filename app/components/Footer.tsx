61947f32edfc44b9270c93231f65528baaf61804
nvrrember <natejc99@gmail.com>
2025-07-24 10:15:16 -0400

    feat: initial commit for home page and footer frameworking

---
+++ app/components/Footer.tsx
@@ -0,0 +1,18 @@
+'use client'
+import React from "react"
+
+export const Footer = () => {
+  return (
+    <footer style={{
+      width: "100%",
+      padding: "1rem 0",
+      background: "#464141ff",
+      color: "#f2f2f2",
+      textAlign: "center"
+    }}>
+      <p><a href="mailto:tech@codecatdevs.com">tech@codecatdevs.com</a></p>
+    </footer>
+  );
+};
+
+export default Footer;
