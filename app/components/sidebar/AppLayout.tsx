c87f3f8637fba6858d7ce2bf906d6abbc23f8acc
Hunter Shaw <huntershaw0@gmail.com>
2025-07-29 10:42:20 -0400

    refactor: changed footer text to emojis

--- app/components/sidebar/AppLayout.tsx
+++ app/components/sidebar/AppLayout.tsx
@@ -53,16 +53,6 @@ const AppLayout = ({children}: AppLayoutProps) => {
                             <h1 className="text-2xl font-semibold text-gray-900">{currentPageTitle}</h1>
                         </div>
 
-                        <div className="flex items-center space-x-4">
-                            <button className="p-2 rounded-md hover:bg-gray-100">
-                                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                          d="M15 17h5l-5 5v-5z"/>
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                          d="M4 19h16a2 2 0 002-2V7a2 2 0 00-2-2H4a2 2 0 00-2 2v10a2 2 0 002 2z"/>
-                                </svg>
-                            </button>
-                        </div>
                     </div>
                 </header>
 
@@ -72,9 +62,11 @@ const AppLayout = ({children}: AppLayoutProps) => {
                 </main>
 
                 <footer
-                    className="bg-white shadow-sm border-t border-gray-200 px-6 py-3 text-gray-900 flex justify-center items-center">Proudly
-                    made by Code Cat Devs
+                    className="bg-white shadow-sm border-t border-gray-200 px-6 py-3 text-gray-900 flex-col justify-center items-center mt-auto">
+                    <div className={'flex justify-center items-center'}>Proudly made by</div>
+                    <div className={'flex justify-center items-center'}>⌨️ 🐱 ☕</div>
                 </footer>
+
             </div>
         </div>
     );
