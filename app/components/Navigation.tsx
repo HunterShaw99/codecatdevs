11f4cab869b4c53836047072af8be79d71fb7561
Hunter Shaw <huntershaw0@gmail.com>
2025-09-05 16:45:20 -0400

    refactor to add optional title for nav component

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
@@ -11,11 +12,17 @@ interface NavigationItem {
 interface NavigationProps {
   items: NavigationItem[];
   className?: string;
+  title?: string;
 }
 
-export const Navigation = ({ items, className = "" }: NavigationProps) => {
+export const Navigation = ({ items, className = "", title }: NavigationProps) => {
   return (
     <div className={`nav-header ${className}`}>
+      {title && (
+        <div className="px-4 sm:px-6 lg:px-8 xl:px-12 2xl:px-16">
+          <h1 className="heading-main">{title}</h1>
+        </div>
+      )}
       <NavigationMenu.Root className="flex justify-center">
         <NavigationMenu.List className="flex space-x-6">
           {items.map((item) => (
